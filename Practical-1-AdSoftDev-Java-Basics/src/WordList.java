7aeeaad52b70a7e75df62d4f5c24f6053eef1593
Wang Shengfan <wang2147@flinders.edu.au>
2023-07-30 04:36:20 +0000

    Checkpoint 3
    git-svn-id: https://topicsvn.flinders.edu.au/svn-repos/COMP8741/wang2147@12 ae434c0c-4b6e-49ec-9e14-c1ab6fc8b626

---
+++ Practical-1-AdSoftDev-Java-Basics/src/WordList.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class WordList {
+    private final ArrayList<String> theWordList = new ArrayList<>();
+
+    /**
+     * Adds a word to this wordlist
+     *
+     * @param s the word to add
+     */
+    public void addWord(String s) {
+        theWordList.add(s);
+    }
+
+    /**
+     * Gets the first word of this WordList,
+     *
+     * @return the first word of the list, or "-" if the list is empty
+     */
+    public String getFirst() {
+        return theWordList.get(0);
+    }
+
+    /**
+     * Gets the last word of this WordList,
+     *
+     * @return the last word of the list, or "-" if the list is empty
+     */
+    public String getLast() {
+        return theWordList.get(size() - 1);
+    }
+
+    /**
+     * @return a String representation of this WordList
+     */
+    public String toString() {
+        return theWordList.toString();
+    }
+
+    /**
+     * @return the number of elements in this WordList
+     */
+    public int size() {
+        return theWordList.size();
+    }
+}
