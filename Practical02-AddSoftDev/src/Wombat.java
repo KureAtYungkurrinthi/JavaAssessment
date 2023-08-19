628c95fb2398aee0d0641458775536c7c7d0f88f
Wang Shengfan <wang2147@flinders.edu.au>
2023-08-19 19:30:46 +0000

    P2T4 wombat clinic queue
    git-svn-id: https://topicsvn.flinders.edu.au/svn-repos/COMP8741/wang2147@308 ae434c0c-4b6e-49ec-9e14-c1ab6fc8b626

---
+++ Practical02-AddSoftDev/src/Wombat.java
@@ -0,0 +1,48 @@
+public class Wombat implements Comparable<Wombat> {
+    private String name;
+    private int severity;
+    private double time;
+
+    public Wombat(String name, int severity, double time) {
+        this.name = name;
+        this.severity = severity;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(int severity) {
+        this.severity = severity;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(Wombat o) {
+        if (this.severity == o.getSeverity())
+            return (int) ((this.time - o.getTime()) * 10);
+        else
+            return o.getSeverity() - this.severity;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + severity + ", " + time;
+    }
+}
---
+++ Practical02-AddSoftDev/src/WombatVetClinic.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class WombatVetClinic {
+    public static void main(String[] args) {
+        Queue<Wombat> wombatQueue = new PriorityQueue<Wombat>();
+
+        wombatQueue.offer(new Wombat("Fluffy", 5, 10.5));
+        wombatQueue.offer(new Wombat("Socks", 10, 11.3));
+        wombatQueue.offer(new Wombat("Barry", 10, 10.8));
+        wombatQueue.offer(new Wombat("Jobe", 3, 12.0));
+        wombatQueue.offer(new Wombat("Ginger", 15, 3.2));
+
+        while (wombatQueue.peek() != null)
+            System.out.println(wombatQueue.poll());
+    }
+}
