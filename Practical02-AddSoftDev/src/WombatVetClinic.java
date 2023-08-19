4af4b5e34493eac0285ee207844f8c579cf38a04
Wang Shengfan <wang2147@flinders.edu.au>
2023-08-19 20:17:29 +0000

    P2T5 get Wombat data from the user via the keyboard.
    git-svn-id: https://topicsvn.flinders.edu.au/svn-repos/COMP8741/wang2147@310 ae434c0c-4b6e-49ec-9e14-c1ab6fc8b626

--- Practical02-AddSoftDev/src/WombatVetClinic.java
+++ Practical02-AddSoftDev/src/WombatVetClinic.java
@@ -1,16 +1,27 @@
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class WombatVetClinic {
-    public static void main(String[] args) throws SeverityOutOfBoundsException {
+    public static void main(String[] args) {
         Queue<Wombat> wombatQueue = new PriorityQueue<Wombat>();
-
-        wombatQueue.offer(new Wombat("Socks", 10, 11.3));
-        wombatQueue.offer(new Wombat("Barry", 10, 10.8));
-        wombatQueue.offer(new Wombat("Jobe", 3, 12.0));
-        wombatQueue.offer(new Wombat("Ginger", 15, 3.2));
-
-        while (wombatQueue.peek() != null)
-            System.out.println(wombatQueue.poll());
+        Scanner in = new Scanner(System.in);
+        boolean notQuit = true;
+        while (notQuit) {
+            String input = in.nextLine().trim();
+            if (input.equalsIgnoreCase("QUIT"))
+                notQuit = false;
+            else if (input.equalsIgnoreCase("PROCESS"))
+                while (wombatQueue.peek() != null)
+                    System.out.println(wombatQueue.poll());
+            else {
+                try {
+                    Scanner line = new Scanner(input);
+                    wombatQueue.add(new Wombat(line.next(), line.nextInt(), line.nextDouble()));
+                } catch (SeverityOutOfBoundsException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 }
