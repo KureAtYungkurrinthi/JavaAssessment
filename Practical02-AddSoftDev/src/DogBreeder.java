c333a8a849371d49a8ed2ee79de5ee7aee3d0cc2
Wang Shengfan <wang2147@flinders.edu.au>
2023-08-19 11:30:57 +0000

    Part 1 Task 4 done
    git-svn-id: https://topicsvn.flinders.edu.au/svn-repos/COMP8741/wang2147@300 ae434c0c-4b6e-49ec-9e14-c1ab6fc8b626

--- Practical02-AddSoftDev/src/DogBreeder.java
+++ Practical02-AddSoftDev/src/DogBreeder.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 public class DogBreeder {
     /**
      * @param args the command line arguments
@@ -11,6 +13,7 @@ public class DogBreeder {
         System.out.println(mydog1);
         System.out.println(mydog2);
 
+        // Task 2
         mydog1.setName("Fido");
         mydog1.addOwner("Anne");
         mydog1.addOwner("Bob");
@@ -20,12 +23,41 @@ public class DogBreeder {
         System.out.println(mydog2.getName());
         Dog.printTotal();
 
-        DogRegister dogRegister = new DogRegister();
-        System.out.println(dogRegister);
-        System.out.println(dogRegister.getDog(3496));
-        dogRegister.addDog(mydog1);
-        dogRegister.addDog(mydog2);
-        System.out.println(dogRegister);
-        System.out.println(dogRegister.getDog(3496));
+        // Task 4
+        System.out.println("Starting dog register");
+
+        // Modify the main method so that it creates a DogRegister object,
+        // adds lots of dogs to it and demonstrates that your methods are correct.
+        DogRegister register = new DogRegister();
+        Dog mydog3 = new Dog("Charlie", "Siberian Husky", 101);
+        Dog mydog4 = new Dog("Bella", "Tugou", 102);
+        Dog mydog5 = new Dog("Luna", "Corgi", 103);
+        Dog mydog6 = new Dog("Lucy", "Shiba Inu", 104);
+        register.addDog(mydog1);
+        register.addDog(mydog2);
+        register.addDog(mydog3);
+        register.addDog(mydog4);
+        register.addDog(mydog5);
+        register.addDog(mydog6);
+        System.out.println(register);
+        Dog.printTotal();
+
+        // Testing retrieving a dog
+        Dog retrievedDog = register.getDog(101);
+        System.out.println("Retrieved dog: " + retrievedDog);
+
+        // Testing deletion
+        Dog deletedDog = register.deleteDog(102);
+        System.out.println("Deleted dog: " + deletedDog);
+        System.out.println("Register after deletion: \n" + register);
+
+        // Testing deletion when no Dog has that registration number
+        Dog nonExistedDog = register.deleteDog(111);
+        System.out.println("Trying to delete non-existed dog: " + nonExistedDog);
+
+        // Testing getting dogs with a specific name sequence
+        Collection<Dog> dogsWithName = register.getDogsWhoseNameContains("Lu");
+        System.out.println("Dogs with names containing 'Lu': " + dogsWithName);
+
     }
 }
--- Practical02-AddSoftDev/src/DogRegister.java
+++ Practical02-AddSoftDev/src/DogRegister.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class DogRegister {
-    private final ArrayList<Dog> dogRegister = new ArrayList<>();
+    private final List<Dog> dogRegister = new ArrayList<>();
 
     // Adds a dog to the register
     public void addDog(Dog dog) {
         dogRegister.add(dog);
+    }
+
+    // Returns the Dog with a registration number of
+    // regNum, if it exists, otherwise returns null.
+    public Dog getDog(int regNum) {
+        for (Dog dog : dogRegister)
+            if (dog.getRegNum() == regNum) return dog;
+        return null;
+    }
+
+    // Returns the current dog register
+    public List<Dog> getRegister() {
+        return dogRegister;
     }
 
     // Removes, and returns, the Dog with a registration number of regNum
@@ -21,20 +36,23 @@ public class DogRegister {
         return null;
     }
 
-    // Returns the Dog with a registration number of
-    // regNum, if it exists, otherwise returns null.
-    public Dog getDog(int regNum) {
+    // Returns a Collection<Dog> containing all the Dogs whose name contains
+    // charSequence.  Does not change the contents of the register.
+    // Include thorough testing code in the main method.
+    public Collection<Dog> getDogsWhoseNameContains(String charSequence) {
+        List<Dog> output = new ArrayList<>();
         for (Dog dog : dogRegister)
-            if (dog.getRegNum() == regNum)
-                return dog;
-        return null;
+            if (dog.getName().contains(charSequence)) output.add(dog);
+        return output;
     }
 
-    // The usual toString method.
+    @Override
     public String toString() {
-        StringBuilder out = new StringBuilder();
-        for (Dog dog : dogRegister)
-            out.append(dog.toString()).append("\n");
-        return out.toString();
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < dogRegister.size(); i++) {
+            output.append(dogRegister.get(i));
+            if (i < dogRegister.size() - 1) output.append("\n");
+        }
+        return output.toString();
     }
 }
