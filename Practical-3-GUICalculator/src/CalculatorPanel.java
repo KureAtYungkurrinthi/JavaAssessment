9dd860a293a0f22aa03dd6e3c29275b6f9feac00
Carl Mooney <carl.mooney@flinders.edu.au>
2023-10-03 04:26:24 +0000

    git-svn-id: https://topicsvn.flinders.edu.au/svn-repos/COMP8741/wang2147@751 ae434c0c-4b6e-49ec-9e14-c1ab6fc8b626

---
+++ Practical-3-GUICalculator/src/CalculatorPanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+/**
+ * The calculator panel
+ *
+ * @author Carl Mooney -- change to your name
+ */
+public class CalculatorPanel extends JPanel implements ButtonConstants {
+
+    /**
+     * Constructor for the Calculator Panel: Sets up the GUI
+     */
+    public CalculatorPanel() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+}
