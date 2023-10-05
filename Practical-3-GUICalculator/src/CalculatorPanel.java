91da550454ca981aecd92dab79af287e8c88d139
Wang Shengfan <wang2147@flinders.edu.au>
2023-10-05 01:57:08 +0000

    Practical 3 Task 1 completed.
    git-svn-id: https://topicsvn.flinders.edu.au/svn-repos/COMP8741/wang2147@777 ae434c0c-4b6e-49ec-9e14-c1ab6fc8b626

--- Practical-3-GUICalculator/src/CalculatorPanel.java
+++ Practical-3-GUICalculator/src/CalculatorPanel.java
@@ -1,11 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * The calculator panel
  *
- * @author Carl Mooney -- change to your name
+ * @author Wang Shengfan
  */
 public class CalculatorPanel extends JPanel implements ButtonConstants {
+    private static final int CALC_WIDTH = 265;
+    private static final int CALC_HEIGHT = 375;
+    private JLabel calculation;
+    private JLabel result;
 
     /**
      * Constructor for the Calculator Panel: Sets up the GUI
@@ -15,7 +20,21 @@ public class CalculatorPanel extends JPanel implements ButtonConstants {
     }
 
     private void init() {
+        setBackground(Color.lightGray);
+        setPreferredSize(new Dimension(CALC_WIDTH, CALC_HEIGHT));
 
+        calculation = new JLabel("", JLabel.RIGHT);
+        calculation.setBackground(Color.lightGray);
+        calculation.setFont(new Font("Helvetica", Font.PLAIN, 12));
+        calculation.setPreferredSize(new Dimension(CALC_WIDTH - 5, 20));
+        add(calculation);
+
+        result = new JLabel("0", JLabel.RIGHT);
+        result.setPreferredSize(new Dimension(CALC_WIDTH - 5, 50));
+        result.setBorder(BorderFactory.createLineBorder(Color.darkGray));
+        result.setFont(new Font("Arial", Font.BOLD, 32));
+        result.setBackground(Color.white);
+        result.setOpaque(true);
+        add(result);
     }
-
 }
